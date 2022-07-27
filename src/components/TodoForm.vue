2a9106944d7efd57f33c252b72690ebdcef24dfa
Ej Sulit <mailbox@helloejsulit.com>
2022-07-27 21:13:23 +0800

    todo list interview submission

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="text" label="Add To Dos" />
+    </div>
+    <q-btn @click="addTodo" color="primary" label="Add Todo" />
+    <q-btn @click="clearText" color="negative" label="Clear" />
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { defineEmits } from 'vue'
+
+const text = ref("")
+const emit = defineEmits(['submit'])
+
+function addTodo() {
+    emit('submit', text.value)
+    text.value = ""
+}
+
+function clearText() {
+  text.value = ""
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+      <TodoForm @submit="addTodo"/>
+      <div>
+        {{ todos }}
+      </div>
+  </q-page>
+</template>
+
+<script setup>
+import TodoForm from '../components/TodoForm.vue'
+import { ref } from 'vue'
+
+const todos = ref([])
+
+
+function addTodo(props) {
+  todos.value.push(props)
+}
+
+
+</script>

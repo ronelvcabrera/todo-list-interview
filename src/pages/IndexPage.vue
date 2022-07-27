9d1d11a0194c163d85adc8edcf0c341d09466d93
Ej Sulit <mailbox@helloejsulit.com>
2022-07-27 22:34:54 +0800

    prepare for edit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,13 @@
 <template>
   <q-page class="cols items-center justify-evenly">
-      <TodoForm @submit="addTodo"/>
+      <TodoForm todo="props.todo" @submit="addTodo"/>
        <q-list bordered separator>
           <q-item clickable v-ripple v-for="(todo,idx) in todos">
             <q-item-section>{{ todo }}</q-item-section>
 
             <q-item-section top side>
               <div class="text-grey-8 q-gutter-xs">
-                <q-btn @click="editTodo(idx)" class="gt-xs" size="12px" flat dense round icon="edit" />
+                <q-btn class="gt-xs" size="12px" flat dense round icon="edit" />
                 <q-btn @click="deleteTodo(idx)" class="gt-xs" size="12px" flat dense round icon="delete" />
               </div>
             </q-item-section>
@@ -21,6 +21,7 @@ import TodoForm from '../components/TodoForm.vue'
 import { ref } from 'vue'
 
 const todos = ref([])
+const props = defineProps(['todo'])
 
 function addTodo(props) {
   todos.value.push(props)
@@ -30,9 +31,7 @@ function deleteTodo(idx) {
   todos.value.splice(idx,1)
 }
 
-function editTodo(idx) {
-  todos.value.splice(idx,1)
-}
+
 
 
 </script>
